ce708a3e0065d73897304aca10585217252c748b
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-02-28 16:52:18 +0000

    push

--- Proyecto_BlackJack.py
+++ Proyecto_BlackJack.py
@@ -12,5 +12,14 @@ if __name__ == "__main__":
     print ("Modos de ejecucion: [J]uego [A]nalisis")
     modoEjecucion = input ("Modo de ejecucion: ")
     
-    
+    if modoEjecucion == 'J':
+        print ("MODO JUEGO SELECCIONADO")
+        
+    if modoEjecucion == 'A':
+        print ("MODO ANALISIS SELECCIONADO")
+        
+    if modoEjecucion != 'J' || modoEjecucion != 'A':
+        print ("Entrada no valida, inserte de nuevo el modo de ejecución")
+        print ("Modos de ejecucion: [J]uego [A]nalisis")
+        modoEjecucion = input ("Modo de ejecucion: ")
     
