3de7106a806044dcc70f049729ed75f6f33543e9
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-02-28 16:46:11 +0000

    push

--- Proyecto_BlackJack.py
+++ Proyecto_BlackJack.py
@@ -1,4 +1,15 @@
 # file:///C:/Users/Alex/Desktop/2%C2%BA%20CUATRI/PAR/PRACTICAS/PRACTICA%201/prac2324a.pdf #
+"""
+Practica 1 - Paradigmas de Programación
+García Lavandera, Alejandro
+García del Caz, Carla
+Curso 2023-2024
+"""
 
-
-
+if __name__ == "__main__":
+    
+    # Pido el modo de ejecucion en el que desea iniciar el programa
+    print ("Modos de ejecucion: [J]uego [A]nalisis")
+    modoEjecucion = input ("Modo de ejecucion: ")
+    
+    
