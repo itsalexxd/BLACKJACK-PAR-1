fc8d99b1369efb18a537f37084d2d99e749f9769
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-01 11:17:35 +0000

    push

--- Proyecto_BlackJack.py
+++ Proyecto_BlackJack.py
@@ -8,3 +8,35 @@ Curso 2023-2024
 
 if __name__ == "__main__":
     
+    # Pedimos el modo de ejecucion en el que desea iniciar el programa
+    print ("Modos de ejecucion: [J]uego [A]nalisis")
+    modoEjecucion = input ("Modo de ejecucion: ")
+    
+    print (" ")
+    
+    numPartida = 0
+    balance = 0
+    
+    if modoEjecucion == 'J':
+        
+        # Sumo 1 a la cuenta de las partidas
+        numPartida += numPartida + 1
+        
+        print ("MODO JUEGO SELECCIONADO")
+        print (" ")
+        print ("--- INICIO DE LA PARTIDA #" + str(numPartida) + " --- BALANCE = " + str(balance) + "€")
+        print (" ")
+        apuesta = input ("Seleccione la apuesta que desea realizar: [2] [10] [50]: ")
+        
+        
+    elif modoEjecucion == 'A':
+        print ("MODO ANALISIS SELECCIONADO")
+    
+    elif modoEjecucion == '':
+        print ("MODO JUEGO PREDETERMINADO")
+        
+    else:
+        print ("Entrada no valida, inserte de nuevo el modo de ejecución")
+        print ("Modos de ejecucion: [J]uego [A]nalisis")
+        modoEjecucion = input ("Modo de ejecucion: ")
+    
