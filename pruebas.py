1d735fa4e30f1b0b04609ba420b33dc0a699f9c9
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-03 18:12:30 +0000

    push

--- pruebas.py
+++ pruebas.py
@@ -11,44 +11,68 @@ def imprimir_carta(palo, valor):
 """
     print(carta)
     
-    
-    
-    
+
 # Indico los rangos para los valores random
 rangoCartasInicio = 1
 rangoCartasFinal = 13
 rangoMazosInicio = 1
-rangoMazosFinal = 4    
+rangoMazosFinal = 4  
 
-def random(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal):
-    randomCartas = random.randint (rangoCartasInicio, rangoCartasFinal)
-    randomMazos = random.randint (rangoMazosInicio, rangoMazosFinal)
+# Funcion para generar dos numeros aleatorios dentro de unos rangos indicados
+def randomizador(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal):
+    randomCartas = random.randint(rangoCartasInicio, rangoCartasFinal)
+    randomMazos = random.randint(rangoMazosInicio, rangoMazosFinal)
     
     return randomCartas, randomMazos
 
-# Genero los valores de forma random dentro de los rangos indicados
-randomCartas, randomMazos = random(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal)
-
-
+# 
 mazoPicas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
 mazoTrevoles = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
 mazoDiamantes = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
 mazoCorazones = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+baraja = [mazoPicas, mazoTrevoles, mazoDiamantes, mazoCorazones]
+
     
 manoCoupier = []
 manoJugador = []
     
     
-def reparteCoupier(randomCartas, randomMazos, mazoPicas, mazoTrevoles, mazoCorazones, manoCoupier):
-    
 
+# Inserto
 def recuperaCartas():
     cartasMazo = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    longitud = len(cartasMazo)
+    long = len(cartasMazo)
     
-    for cartas in range longitud:
-           mazoCorazones.append(0)
+    for i in range (long):
+        # Recorro el array con los valores de las cartas para guardarlo en la variable "restaurador"
+        restaurador = cartasMazo.append(i)
+        # Inserto el valor de la variable restaurador en las arrays
+        mazoPicas.append(i, restaurador)
+        mazoTrevoles.append(i, restaurador)
+        mazoDiamantes.append(i, restaurador)
+        mazoCorazones.append(i, restaurador)
 
+# def reparteCoupier(randomCartas, randomMazos, mazoPicas, mazoTrevoles, mazoCorazones, manoCoupier):
 
+# Funcion para mostrar la baraja completa
+def muestraBaraja(baraja):
+    for i, mazo in enumerate(baraja):
+        print(f"Mazo {i + 1}: ", end="")
+        print(*mazo, sep=", ")
+    
+    
+    
 if __name__ == "__main__":
     
+    # Genero 2 numeros de forma aleatoria para elegir mazo y carta
+    # randomCartas, randomMazos = randomizador(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal)
+
+    mazoPicas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+    mazoTrevoles = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+    mazoDiamantes = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+    mazoCorazones = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+    baraja = [mazoPicas, mazoTrevoles, mazoDiamantes, mazoCorazones]
+    
+    muestraBaraja(baraja)
