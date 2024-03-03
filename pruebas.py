066df9cf28acf3b6bcbda48033c9425caaa53f83
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-03 13:06:11 +0000

    push

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,5 @@
+import random
+
 def imprimir_carta(palo, valor):
     carta = f"""
 ╭───────╮
@@ -8,6 +10,45 @@ def imprimir_carta(palo, valor):
 ╰───────╯
 """
     print(carta)
+    
+    
+    
+    
+# Indico los rangos para los valores random
+rangoCartasInicio = 1
+rangoCartasFinal = 13
+rangoMazosInicio = 1
+rangoMazosFinal = 4    
 
-# Ejemplo de uso
-imprimir_carta("♥", "J")
+def random(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal):
+    randomCartas = random.randint (rangoCartasInicio, rangoCartasFinal)
+    randomMazos = random.randint (rangoMazosInicio, rangoMazosFinal)
+    
+    return randomCartas, randomMazos
+
+# Genero los valores de forma random dentro de los rangos indicados
+randomCartas, randomMazos = random(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal)
+
+
+mazoPicas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+mazoTrevoles = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+mazoDiamantes = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+mazoCorazones = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+    
+manoCoupier = []
+manoJugador = []
+    
+    
+def reparteCoupier(randomCartas, randomMazos, mazoPicas, mazoTrevoles, mazoCorazones, manoCoupier):
+    
+
+def recuperaCartas():
+    cartasMazo = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
+    longitud = len(cartasMazo)
+    
+    for cartas in range longitud:
+           mazoCorazones.append(0)
+
+
+if __name__ == "__main__":
+    
