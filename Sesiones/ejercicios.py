70b81de8ad2cb009538f352ea13f7b72f84c58e5
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-04 09:12:02 +0000

    push

--- Sesiones/ejercicios.py
+++ Sesiones/ejercicios.py
@@ -1,10 +1,21 @@
-def verMatriz(matriz):
+import math
+
+def 
+
+def verMatriz():
+    print ("###############################")
+    print ("#### E J E R C I C I O - 6 ####")
+    print ("###############################")
+    
+    matriz = [[4,1,5],[3,2,4],[9,0,1]]
     for i in range(3):
         for j in range(3):
-            print (matriz[i][j], end="")
+            print (matriz[i][j], end=" ")
+        print()
 
 
-if __name__ == "__name__":
-    matriz = [[4,1,5], [3,2,4], [9,0,1]]
-    verMatriz(matriz)
+if __name__ == "__main__":
+    
+    # Ejercicio 6
+    verMatriz()
     
