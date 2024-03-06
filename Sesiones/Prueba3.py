e85d0a2d5a6792137182df8de430cc4f926ba057
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-06 15:43:52 +0000

    push

--- Sesiones/Prueba3.py
+++ Sesiones/Prueba3.py
@@ -1,20 +1,32 @@
-# Sesion 3 -> Tratamiento de ficheros
+# Sesion 3 -> Tratamiento de ficheros y excepciones
 
 # Ejemplo de lectura de ficheros
-'''
-
-'''
 
 def Main():
-    # Apertura del fichero
-    with open ('ejemplo.txt', 'r') as fichero:
-        # Leer contenido del fichero
-        contenido = fichero.read()
-        print (contenido)
+    # Tratamiento de excepciones a la hora de abrir un fichero
+    try:
+        # Apertura del fichero (modo lectura -> r)
+        with open('/workspaces/BLACKJACK-PAR-1/Sesiones/ejemplo.txt','r') as fichero:
+            # Leer contenido del fichero
+            contenido = fichero.read()
+            print (contenido)
+        # El fichero se cierra de forma automatica una vez acaba la indentacion del with
 
+    except IOError as e:
+        # En esta parte del codigo, hubo un error y los mostramos por pantalla
+        print (f"Error a la hora de abrir el fichero: {e}")
+        
+        
+    try:
+        with open('/workspaces/BLACKJACK-PAR-1/numeros.txt','w') as file:
+            # Escribo el contenido en el archivo
+            for i in range (0,201,2):
+                file.write(f"{i}\n")
+            
+    except IOError as error:
+        print("Error a la hora de abrir el archivo: {error}")
     
 
 if __name__ == "__main__":
     Main()
 
-    
