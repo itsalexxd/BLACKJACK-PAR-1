70d623bd2883e7fd2d3ae463fee95980801008c8
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-19 21:01:23 +0000

    push

--- pruebas.py
+++ pruebas.py
@@ -1,132 +1,158 @@
+"""
+Practica 1 - Paradigmas de Programacion
+Garcí­a Lavandera, Alejandro
+Garcí­a del Caz, Carla
+Curso 2023-2024
+"""
+
 import externo
-import random
-
-def imprimir_carta(palo, valor):
-    carta = f"""
-╭───────╮
-│       │
-│    {valor} │
-│       │
-│ {palo}     │
-│       │
-╰───────╯
-    """
-    print(carta)
-    
-
-
-
-# Funcion para generar dos numeros aleatorios dentro de unos rangos indicados
-def randomizador(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal):
-    randomCartas = random.randint(rangoCartasInicio, rangoCartasFinal)
-    randomMazos = random.randint(rangoMazosInicio, rangoMazosFinal)
-    
-    return randomCartas, randomMazos
-
-# Recupero el valor de las cartas en la baraja
-def recuperaCartas():
-    cartasMazo = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    long = len(cartasMazo)
-    
-    for i in range (long):
-        # Recorro el array con los valores de las cartas para guardarlo en la variable "restaurador"
-        restaurador = cartasMazo.append(i)
-        # Inserto el valor de la variable restaurador en las arrays
-        mazoPicas.append(i, restaurador)
-        mazoTrevoles.append(i, restaurador)
-        mazoDiamantes.append(i, restaurador)
-        mazoCorazones.append(i, restaurador)
-
-# Funcion para mostrar la baraja completa
-def muestraBaraja(baraja):
-    for i, mazo in enumerate(baraja):
-        print(f"Mazo {i + 1}: ", end="")
-        print(*mazo, sep=", ")
-        
-class Mano:
-    def __init__(self):
-        # self.estado = estado    # 3 estados -> Abierta/Cerrada/Pasada o Separar
-        # self.valor = valor      # Suma de las cartas de la mano
-        # self.apuesta = apuesta  # Valor de la apuesta de la mano
-        self.cartas = []        # Cartas que posee la mano
-        
-    def agregarCartas(self, carta):
-        self.cartas.append(carta)
-        
-    def obtenerSuma(self):
-        valorTotal = 0
-        ases = 0
-        for carta in self.cartas:
-            if carta in ['J', 'Q', 'K']:
-                valorTotal += 10
-            elif carta == 'A':
-                ases += 1
-            else:
-                valorTotal += int(carta)
-        
-        for _ in range(ases):
-            if valorTotal + 11 <= 21:
-                valorTotal += 11
-            else:
-                valorTotal += 1
-        
-        return valorTotal
-
-class Croupier:
-    def __init__(self):
-        self.mano = Mano()
-
-class Jugador:
-    def __init__(self):
-        self.manos = [Mano(),Mano()]
-        
     
 def Main():
-        # Indico los rangos para generar los valores random
-    rangoCartasInicio = 1
-    rangoCartasFinal = 13
-    rangoMazosInicio = 1
-    rangoMazosFinal = 4  
-    # Genero 2 numeros de forma aleatoria para elegir mazo y carta
-    randomCartas, randomMazos = randomizador(rangoCartasInicio, rangoCartasFinal, rangoMazosInicio, rangoMazosFinal)
-    print ("Cartas: ", randomCartas," Mazos: ", randomMazos)
+    #elegir modo de juego
+    continuar_modo=True
     
-
-    # Creo la baraja
-    mazoPicas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    mazoTrevoles = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    mazoDiamantes = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    mazoCorazones = ['1','2','3','4','5','6','7','8','9','10','J','Q','K']
-    baraja = [mazoPicas, mazoTrevoles, mazoDiamantes, mazoCorazones]
-    # Mostramos la baraja
-    muestraBaraja(baraja)
-
-    # Mostramos por pantalla las cartas
-    imprimir_carta("♦", " J")
-    imprimir_carta("♦", "10")    
-    
-    # Definimos 
-    croupier = Croupier()
-    jugador = Jugador()
-    
-    # Agregamos cartas a las manos
-    croupier.mano.agregarCartas('J')
-    
-    jugador.manos[0].agregarCartas('K')
-    
-    jugador.manos[1].agregarCartas('A')
-    jugador.manos[1].agregarCartas('A')
-    
-    # Mostramos el valor total de cada mano
-    print("Valor total de la mano del croupier:", croupier.mano.obtenerSuma())
-    print("Valor total de la mano del jugador:", jugador.manos[0].obtenerSuma())
-    print("Valor total de la mano del jugador:", jugador.manos[1].obtenerSuma())
-
-
-
-    externo.Mazo()
-
-
-    
-if __name__ == "__main__":
+    while continuar_modo:
+        ejecucion=input('¿Modo de ejecución? [J]uego [A]nálisis: ')
+        ejecucion.upper()
+        
+        if ejecucion=='J':
+            '''
+            ejecutar modo juego
+                (pedir apuesta)
+                (iniciar bucle while que se rompe cuando la respuesta es no)
+                (hacer balance)
+                (mostrar todos los resultados al final)
+            '''
+            continuar_modo= False
+            
+            partida=0
+            balance=0
+            continuar_partida= True
+            apuesta_correcta=True
+            respuesta_correcta=True
+            continuar_jugador=True
+            continuar_croupier=True
+            jugada_correcta=True
+                
+            while continuar_partida:
+                partida=+1
+                print(f'--- INICIO DE LA PARTIDA {partida}--- BALANCE {balance} € ')
+                    
+                while apuesta_correcta:
+                    apuesta=input('¿Apuesta?')
+                        
+                    if apuesta==2:
+                        apuesta_correcta= False
+                        
+                    elif apuesta==10:
+                        apuesta_correcta= False
+                        
+                    elif apuesta==50:
+                        apuesta_correcta=False
+                        
+                    else:
+                        apuesta_correcta=True
+                        
+                print('REPARTO INICIAL')
+                #repartir cartas
+                
+                
+                #if valor de las cartas son iguales y solo hay dos cartas
+                while jugada_correcta:
+                    jugada=input('¿Jugada para mano? [P]edir [D]oblar [C]errar [S]eparar')
+                    jugada.upper()
+                            
+                    if jugada=='P' or jugada=='D' or jugada=='C' or jugada=='S':
+                        jugada_correcta==False
+                        
+                    else:
+                        jugada_correcta==True
+                            
+                while continuar_jugador:
+                    if jugada=='P':
+                        #dar otra carta
+                        #evaluar estado mano(pasada o abierta)
+                        continuar_jugador=True
+                        
+                    elif jugada=='D':
+                        apuesta=apuesta*2
+                        #dar otra carta
+                        #evaluar estado mano (pasada o cerrada)
+                        continuar_jugador=False
+                                
+                    elif jugada=='C':
+                        #cambiar estado de mano(cerrada)
+                        continuar_jugador=False
+                            
+                    elif jugada=='S':
+                        #crear dos manos, una con la primera carta y la otra con la segunda
+                        #estado de mano abierto
+                        continuar_jugador=True
+                            
+                while continuar_croupier:
+                    '''
+                    -todas las manos del jugador pasadas, no se piden cartas
+                        -contar resultados
+                    -caso contrario
+                        -pedir hasta >= 17
+                    '''
+                        
+                #contabilizar resultados
+                    '''
+                    -Ambas manos pasadas o mismo valor: ninguno obtiene beneficio
+                    -croupier pasada o <jugador: el croupier paga al jugador la apuesta
+                    -jugador pasada o <croupier: el jugador paga
+                        
+                    -resultado: suma de todas las manos
+                    '''
+                        
+                while respuesta_correcta:
+                    respuesta=input('¿Otra partida? [S/N]')
+                    respuesta.upper()
+                        
+                    if respuesta=='S' or respuesta=='N':
+                        respuesta_correcta=False
+                            
+                    else:
+                        respuesta_correcta=True
+                            
+                if respuesta=='S':
+                    continuar_partida=True
+                        
+                else:
+                    print('BALANCE FINAL{}')
+                    continuar_partida=False
+                        
+                        
+                            
+                
+                
+                
+                
+                
+        
+        elif ejecucion=='A':
+            '''
+            ejecutar modo analisis
+                (pedir número de partidas)
+                (hacer balance)
+                (mostrar todos los resultados al fina)
+            '''
+            continuar_modo= False
+                
+        elif ejecucion=='':
+            '''
+            ejecutar modo juego
+            apuesta 10
+            primera accion pedir
+            jugar otra partida
+            '''
+            continuar_modo=False
+                
+        else:
+            continuar_modo=True
+        
+        
+ 
+if __name__=='__main__':
     Main()
