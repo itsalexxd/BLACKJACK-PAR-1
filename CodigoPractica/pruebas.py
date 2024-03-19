1bc8a76cbaa7bd2ca36efdc2bb2f8627213d7a94
aalexgarc_ <100075644+itsalexxd@users.noreply.github.com>
2024-03-19 12:54:40 +0000

    push

--- CodigoPractica/pruebas.py
+++ CodigoPractica/pruebas.py
@@ -1,4 +1,5 @@
 import externo2
+import os
 
 # PICAS TREVOLES DIAMANTES CORAZONES ♠  ♥  ♦  ♣#
 # A-10 y (J Q K) = 10
@@ -53,6 +54,12 @@ class MiCarta(externo2.CartaBase):
             """
             print(carta)
 
+def clearTermina():
+    os.system('clear')
+    
+def separaciones():
+    for i in range (2):
+        print()
 
 #################
 #### M A I N ####
@@ -60,18 +67,56 @@ class MiCarta(externo2.CartaBase):
 
 def Main():
        
-    estrategia = externo2.Estrategia(2)
-    mazo = externo2.Mazo(MiCarta,estrategia)
+    # estrategia = externo2.Estrategia(2)
+    # mazo = externo2.Mazo(MiCarta,estrategia)
 
 
-    listaCartas = []
+    # listaCartas = []
     
-    # Calculo e inserto el indice de las cartas en listaCartas
-    for i in range(100):
-        valor = int(mazo.reparte().ind)
-        listaCartas.append(valor) 
+    # # Calculo e inserto el indice de las cartas en listaCartas
+    # for i in range(51):
+    #     valor = int(mazo.reparte().ind)
+    #     listaCartas.append(valor) 
     
-    print(listaCartas)
+    # for i in range(5):
+    #     carta = MiCarta(listaCartas[i])
+    #     carta.dibujacarta(carta)
+        
+    # print(listaCartas)    
+    
+    
+    
+    # LIMPIO EL TERMINAL #
+    clearTermina()
+    
+    
+    # INICIAMOS EL JUEGO PIDIENDO MODO DE EJECUCION #
+    print("Indique el modo de ejecucion:")
+    modoEjecucion = input("[J]uego [A]nalisi:")
+    
+    bucle1 = False
+    
+    while bucle1 == False:
+        if modoEjecucion == "J" or modoEjecucion == "j":
+            bucle1 = True
+            separaciones()
+            print("### MODO JUEGO SELECCIONADO ###")
+        
+        elif modoEjecucion == "A" or modoEjecucion == "a":
+            bucle1 = True
+            separaciones()
+            print("### MODO ANALISIS SELECCIONADO ###")
+    
+        elif modoEjecucion == "":
+            bucle1 = True
+            separaciones()
+            print("### MODO JUEGO PREDETERMINADO ###")
+        else:
+            separaciones()
+            print("Opcion insertada no valida, vuelva a insertar el modo de ejecucion")
+            modoEjecucion = input("[J]uego [A]nalisi:")
+    
+    
 
 if __name__ == "__main__":
     Main()
